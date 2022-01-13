838ae710089dee2a5ced15da5262675db488b3eb
Jay <bjw1622@gmail.com>
2022-01-13 21:11:16 +0900

    var의 개념 알기

--- 변수 다루기/1. var를 지양하자.js
+++ 변수 다루기/1. var를 지양하자.js
@@ -1,7 +1,3 @@
-// settings.json 수정
-// "editor.bracketPairColorization.enabled": true,
-// "editor.guides.bracketPairs":"active"
-
 // var => 함수 스코프
 // let, const => 블록 스코프
 // let => 재할당 O
@@ -10,14 +6,14 @@
 var global = '전역';
 
 if (global === '전역') {
-    var global = '지역'
-    console.log(global);
+  var global = '지역';
+  console.log(global);
 }
 
 let letGlobal = '전역';
 
 if (letGlobal === '전역') {
-    let letGlobal = '지역'
-    console.log(letGlobal);
+  let letGlobal = '지역';
+  console.log(letGlobal);
 }
-console.log(letGlobal);  // let, const는 블록 스코프이므로 
+console.log(letGlobal); // let, const는 블록 스코프이므로
