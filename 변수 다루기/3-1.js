54fecc0ad5e54d76e684d9fb51b88b6625c13cbd
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-13 15:49:42 +0900

    전역 변수의 위험성

--- 변수 다루기/3-1.js
+++ 변수 다루기/3-1.js
@@ -1,3 +1,9 @@
 const global = "global";
 console.log(global);
-console.log(window);
+
+// 웹 API 이기 떄문에 코드 작성시에 에러 발생 X
+const setTimeout = 'setTimeout';
+
+function setTimeout() {
+    console.log('function');
+}
