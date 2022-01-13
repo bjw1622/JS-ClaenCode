7556e0cff9413dac16c3bfbf08727bc5a39a0ff3
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-13 16:42:39 +0900

    임시 변수 제거하기

--- 변수 다루기/3-1.js
+++ 변수 다루기/3-1.js
@@ -4,6 +4,6 @@ console.log(global);
 // 웹 API 이기 떄문에 코드 작성시에 에러 발생 X
 const setTimeout = 'setTimeout';
 
-function setTimeout() {
-    console.log('function');
-}
+// function setTimeout() {
+//     console.log('function');
+// }
--- 변수 다루기/3-2.js
+++ 변수 다루기/3-2.js
@@ -1,6 +1,6 @@
 console.log(global);
 
-setTimeout(() => {
-    console.log('1초');
-}, 1000);
+// setTimeout(() => {
+//     console.log('1초');
+// }, 1000);
 // 3-1에서 만든 setTimeOut변수로 인해 동작 x
